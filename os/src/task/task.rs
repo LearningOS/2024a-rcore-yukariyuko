9313a424e6f4d24fa51003d6f40ab9a71b496f99
miiyakumo <miiyakumo@qq.com>
2024-11-07 21:24:44 +0800

    finish span and priority, add reoprt3

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -69,6 +69,9 @@ pub struct TaskControlBlockInner {
     /// Program break
     pub program_brk: usize,
 
+    /// priority
+    pub priority: isize,
+
     /// syscall tiems
     pub syscalls: Vec<(usize, u32)>,
 
@@ -124,6 +127,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    priority: 16,
                     syscalls: Vec::new(),
                     time_stamp: 0,
                 })
@@ -199,6 +203,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: parent_inner.heap_bottom,
                     program_brk: parent_inner.program_brk,
+                    priority: parent_inner.priority,
                     syscalls: parent_inner.syscalls.clone(),
                     time_stamp: parent_inner.time_stamp,
                 })
